3cabb8a94761843be7999c37945816e8d5c749e3
denis909 <denis909@mail.ru>
2021-04-15 15:32:10 +0300

    n/a

--- ActiveRecord.php
+++ ActiveRecord.php
@@ -2,11 +2,23 @@
 
 namespace denis909\yii;
 
+use Psr\Log\LoggerAwareTrait;
+use Psr\Log\NullLogger;
+
 class ActiveRecord extends \yii\db\ActiveRecord implements ModelInterface
 {
 
     use ModelTrait;
     
     use ActiveRecordTrait;
+
+    use LoggerAwareTrait;
+
+    public function __construct($config = [])
+    {
+        parent::__construct($config);
+
+        $this->setLogger(new NullLogger);
+    }
 
 }
